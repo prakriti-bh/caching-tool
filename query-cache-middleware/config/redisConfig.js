27934658ac45d9153c8d38519637ff5901caefd4
Aryan <aryan.bhirud@hotmail.com>
2024-09-26 10:31:52 +0530

    Integrated multiple caching features

--- query-cache-middleware/config/redisConfig.js
+++
@@ -1,7 +0,0 @@
-// config/redisConfig.js
-
-require('dotenv').config(); // Load environment variables from .env
-
-module.exports = {
-    redisUrl: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
-};
